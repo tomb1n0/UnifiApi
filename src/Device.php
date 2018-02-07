9d117c9727e38cd5e1fe1cc0adbadd2adbe8c7cc
Tom Harper <tom@sticktech.co.uk>
2018-02-07 14:58:18 +0000

    Add support for automatically updating the firmware for a device

--- src/Device.php
+++ src/Device.php
@@ -5,7 +5,6 @@ namespace UnifiAPI;
 class Device extends UnifiElement {
 
 	public function adopt() {
-		// first of all check if we're already adopted
 		if ($this->adopted) {
 			throw new \Exception('Device ' . $this->mac . ' already adopted');
 		}
@@ -14,6 +13,17 @@ class Device extends UnifiElement {
 
 	public function forget() {
 		$this->api->post('/api/s/' . $this->site_id . '/cmd/sitemgr', ['mac' => $this->mac, 'cmd' => 'delete-device']);
+	}
+
+	public function upgrade() {
+		$firmwares = $this->api->post('/api/s/' . $this->site_id . '/cmd/firmware', ['cmd' => 'list-cached']);
+		foreach ($firmwares as $firmware) {
+			if ($firmware->device == $this->model) {
+				if ($firmware->version != $this->version) {
+					$this->api->post('/api/s/' . $this->site_id . '/cmd/devmgr', ['mac' => $this->mac, 'upgrade_to_firmware' => $firmware->version, 'cmd' => 'upgrade']);
+				}
+			}
+		}
 	}
 
 }
