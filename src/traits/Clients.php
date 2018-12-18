1633529deb14565dd8257ef49a1898a716a0520b
Tom Harper <tom@sticktech.co.uk>
2018-12-18 16:13:46 +0000

    Implement a general way of filtering clients

--- src/traits/Clients.php
+++ src/traits/Clients.php
@@ -15,16 +15,25 @@ trait Clients {
 		);
 	}
 
-	public function online_clients_for_vlan($vlan_id) {
+	public function filter_clients($key, $value) {
 		$raw_clients = $this->api->get('/api/s/' . $this->site_id . '/stat/sta');
-		return array_map(
+		$data = array_map(
 			function ($client) {
 				return new Client($client, $this->api);
 			},
-			array_filter($raw_clients, function ($client) use ($vlan_id) {
-				return isset($client->vlan) && $client->vlan == (int) $vlan_id;
+			array_filter($raw_clients, function ($client) use ($key, $value) {
+				return isset($client->$key) && $client->$key == $value;
 			})
 		);
+		return array_values($data);
+	}
+
+	public function online_clients_for_vlan($vlan_id) {
+		return $this->filter_clients('vlan', (int) $vlan_id);
+	}
+
+	public function online_clients_for_ssid($ssid_name) {
+		return $this->filter_clients('essid', $ssid_name);
 	}
 
 }
