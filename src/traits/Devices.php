77960c5642768fb065d055251404de779d135dfa
Tristan Harris <tristan@sticktech.co.uk>
2020-08-24 18:06:59 +0100

    Reduce multiple API calls when building all_devices

--- src/traits/Devices.php
+++ src/traits/Devices.php
@@ -9,7 +9,8 @@ use GuzzleHttp\Exception\ClientException;
 trait Devices {
 
 	public function device($mac_address, $wanted_fields = []) {
-		return $this->build_device($mac_address, $wanted_fields);
+		$devices = $this->build_devices([$mac_address], $wanted_fields);
+		return $devices[0];
 	}
 
 	private function devices_basic() {
@@ -18,18 +19,18 @@ trait Devices {
 
 	public function all_devices($wanted_fields = []) {
 		$devices = $this->devices_basic();
-		return array_map(function ($device) use ($wanted_fields) {
-			return $this->build_device($device->mac, $wanted_fields);
+		$macs = array_map(function ($device) {
+			return $device->mac;
 		}, $devices);
+		return $this->build_devices($macs, $wanted_fields);
 	}
 
-	private function build_device($mac, $wanted_fields) {
+	private function build_devices($macs, $wanted_fields) {
 		try {
-			list($full_info) = $this->api->get('/api/s/' . $this->site_id . '/stat/device', ['macs' => [$mac]]);
+			$devices = $this->api->get('/api/s/' . $this->site_id . '/stat/device', ['macs' => $macs]);
 		} catch (ClientException $e) {
 			return null;
 		}
-		$device = new \StdClass;
 		// always have fields used by this library in the wanted fields by default
 		$wanted_fields = array_unique(
 			array_merge(
@@ -37,21 +38,25 @@ trait Devices {
 				['model', 'mac', 'switch_vlan_enabled', '_id', 'adopted', 'version']
 			)
 		);
-		foreach ($wanted_fields as $key) {
-			if (isset($full_info->$key)) {
-				$device->$key = $full_info->$key;
+		return array_map(function ($full_info) use ($wanted_fields) {
+			$device = new \StdClass;
+			foreach ($wanted_fields as $key) {
+				if (isset($full_info->$key)) {
+					$device->$key = $full_info->$key;
+				}
 			}
-		}
-		switch ($device->model) {
-			case 'U2IW':
-			case 'U7IW':
-			case "U7IWP":
-			return new WallPlateAP($device, $this->api);
-			break;
-			default:
-			return new Device($device, $this->api);
-			break;
-		}
+			switch ($device->model) {
+				case 'U2IW':
+				case 'U7IW':
+				case "U7IWP":
+				return new WallPlateAP($device, $this->api);
+				break;
+				default:
+				return new Device($device, $this->api);
+				break;
+			}
+			return $device;
+		}, $devices);
 	}
 
 	public function unadopted_devices() {
