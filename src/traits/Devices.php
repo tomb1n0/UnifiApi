fb95528492686787b34acef8c280fcf9ad71c1ad
Tristan Harris <tristan@sticktech.co.uk>
2020-08-25 16:07:30 +0100

    all_devices: Add chunk size to control memory usage vs performance

--- src/traits/Devices.php
+++ src/traits/Devices.php
@@ -9,7 +9,7 @@ use GuzzleHttp\Exception\ClientException;
 trait Devices {
 
 	public function device($mac_address, $wanted_fields = []) {
-		$devices = $this->build_devices([$mac_address], $wanted_fields);
+		$devices = $this->build_devices([$mac_address], $wanted_fields, 1);
 		return $devices[0];
 	}
 
@@ -17,20 +17,17 @@ trait Devices {
 		return $this->api->get('/api/s/' . $this->site_id . '/stat/device-basic');
 	}
 
-	public function all_devices($wanted_fields = []) {
+	//$chunk_size allows tradeoff between max memory usage and number of api requests(speed)
+	// smaller -> more memory efficient but slower
+	public function all_devices($wanted_fields = [], $chunk_size = 40) {
 		$devices = $this->devices_basic();
 		$macs = array_map(function ($device) {
 			return $device->mac;
 		}, $devices);
-		return $this->build_devices($macs, $wanted_fields);
+		return $this->build_devices($macs, $wanted_fields, $chunk_size);
 	}
 
-	private function build_devices($macs, $wanted_fields) {
-		try {
-			$devices = $this->api->get('/api/s/' . $this->site_id . '/stat/device', ['macs' => $macs]);
-		} catch (ClientException $e) {
-			return null;
-		}
+	private function build_devices($all_macs, $wanted_fields, $chunk_size) {
 		// always have fields used by this library in the wanted fields by default
 		$wanted_fields = array_unique(
 			array_merge(
@@ -38,25 +35,35 @@ trait Devices {
 				['model', 'mac', 'switch_vlan_enabled', '_id', 'adopted', 'version']
 			)
 		);
-		return array_map(function ($full_info) use ($wanted_fields) {
-			$device = new \StdClass;
-			foreach ($wanted_fields as $key) {
-				if (isset($full_info->$key)) {
-					$device->$key = $full_info->$key;
+		$out = [];
+		foreach (array_chunk($all_macs, $chunk_size) as $macs) {
+			try {
+				$devices = $this->api->get('/api/s/' . $this->site_id . '/stat/device', ['macs' => $macs]);
+			} catch (ClientException $e) {
+				return null;
+			}
+			foreach ($devices as $full_info) {
+				$device = new \StdClass;
+				foreach ($wanted_fields as $key) {
+					if (isset($full_info->$key)) {
+						$device->$key = $full_info->$key;
+					}
 				}
+				switch ($device->model) {
+					case 'U2IW':
+					case 'U7IW':
+					case "U7IWP":
+						$dev = new WallPlateAP($device, $this->api);
+						break;
+					default:
+						$dev = new Device($device, $this->api);
+						break;
+				}
+				$out[] = $dev;
 			}
-			switch ($device->model) {
-				case 'U2IW':
-				case 'U7IW':
-				case "U7IWP":
-				return new WallPlateAP($device, $this->api);
-				break;
-				default:
-				return new Device($device, $this->api);
-				break;
-			}
-			return $device;
-		}, $devices);
+			unset($devices);
+		}
+		return $out;
 	}
 
 	public function unadopted_devices() {
